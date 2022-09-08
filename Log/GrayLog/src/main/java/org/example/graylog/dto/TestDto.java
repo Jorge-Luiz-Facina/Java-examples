bd00317872012480c209919eb2763c8302390827
Jorge Luiz Facina <mainduelo@gmail.com>
2022-09-08 00:34:42 -0300

    update dto and pom

--- Log/GrayLog/src/main/java/org/example/graylog/dto/TestDto.java
+++ Log/GrayLog/src/main/java/org/example/graylog/dto/TestDto.java
@@ -1,10 +1,33 @@
 package org.example.graylog.dto;
 
+<<<<<<< HEAD
 import lombok.Getter;
 
 @Getter
+=======
+<<<<<<< HEAD
+import lombok.Getter;
+
+@Getter
+=======
+>>>>>>> 6bffeb2 (add example log graylog mask)
+>>>>>>> 3aa6a63 (update dto and pom)
 public class TestDto {
 
     private String cpf = "CPF";
     private String cnpj = "CNPJ";
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+>>>>>>> 6bffeb2 (add example log graylog mask)
+>>>>>>> 3aa6a63 (update dto and pom)
 }

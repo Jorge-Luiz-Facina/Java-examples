fa2da02b7926d4c84b51841f53d475038e4df5ab
Jorge Luiz Facina <mainduelo@gmail.com>
2022-09-08 22:42:20 -0300

    resolved conflict

--- Log/GrayLog/src/main/java/org/example/graylog/dto/TestDto.java
+++ Log/GrayLog/src/main/java/org/example/graylog/dto/TestDto.java
@@ -1,33 +1,12 @@
 package org.example.graylog.dto;
 
-<<<<<<< HEAD
 import lombok.Getter;
 
-@Getter
-=======
-<<<<<<< HEAD
-import lombok.Getter;
 
 @Getter
-=======
->>>>>>> 6bffeb2 (add example log graylog mask)
->>>>>>> 3aa6a63 (update dto and pom)
 public class TestDto {
 
     private String cpf = "CPF";
     private String cnpj = "CNPJ";
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
->>>>>>> 6bffeb2 (add example log graylog mask)
->>>>>>> 3aa6a63 (update dto and pom)
 }
